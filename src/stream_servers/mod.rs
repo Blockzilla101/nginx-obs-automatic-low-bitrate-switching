091265f5e2b89372957541198bc2559c1ccfa407
Blockzilla101 <blockzilla101@gmail.com>
2024-05-26 10:48:38 +0500

    support srs

--- src/stream_servers/mod.rs
+++ src/stream_servers/mod.rs
@@ -13,6 +13,7 @@ pub mod nms;
 pub mod obs;
 pub mod rist;
 pub mod sls;
+pub mod srs;
 pub mod xiu;
 
 pub use belabox::Belabox;
@@ -23,6 +24,7 @@ pub use nms::NodeMediaServer;
 pub use obs::Obs;
 pub use rist::Rist;
 pub use sls::SrtLiveServer;
+pub use srs::SRS;
 pub use xiu::Xiu;
 
 #[async_trait]
---
+++ src/stream_servers/srs.rs
@@ -0,0 +1,137 @@
+use std::string::String;
+
+use async_trait::async_trait;
+use log::{error, trace};
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+
+use super::{default_reqwest_client, Bsl, StreamServersCommands, SwitchLogic};
+use crate::switcher::{SwitchType, Triggers};
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct StatKbps {
+    recv_30s: u64,
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Stat {
+    pub id: String,
+    pub name: String,
+    pub send_bytes: u64,
+    pub recv_bytes: f64,
+    pub live_ms: u64,
+    pub kbps: StatKbps,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct SRS {
+    /// URL to SRS stats page (ex; http://127.0.0.1:1985/api/v1/streams )
+    pub stats_url: String,
+
+    /// StreamID of the where you are publishing the feed. (ex; publish/live/<feed1> )
+    pub publisher: String,
+
+    /// Client to make HTTP requests with
+    #[serde(skip, default = "default_reqwest_client")]
+    pub client: reqwest::Client,
+}
+
+impl SRS {
+    pub async fn get_stats(&self) -> Option<Stat> {
+        let res = match self.client.get(&self.stats_url).send().await {
+            Ok(res) => res,
+            Err(_) => {
+                error!("Stats page ({}) is unreachable", self.stats_url);
+                return None;
+            }
+        };
+
+        if res.status() != reqwest::StatusCode::OK {
+            error!("Error accessing stats page ({})", self.stats_url);
+            return None;
+        }
+
+        let text = res.text().await.ok()?;
+        let data: Value = serde_json::from_str(&text).ok()?;
+        let publisher = data["streams"]
+            .as_array()?
+            .iter()
+            .find(|x| x["name"] == self.publisher)?;
+
+        let stream: Stat = serde_json::from_value(publisher.to_owned()).ok()?;
+        // let stream: Stat = match serde_json::from_value(publisher.to_owned()) {
+        //     Ok(stats) => stats,
+        //     Err(error) => {
+        //         trace!("{}", &data);
+        //         error!("Error parsing stats ({}) {}", self.stats_url, error);
+        //         return None;
+        //     }
+        // };
+
+        trace!("{:#?}", stream);
+        Some(stream)
+    }
+}
+
+#[async_trait]
+#[typetag::serde]
+impl SwitchLogic for SRS {
+    async fn switch(&self, triggers: &Triggers) -> SwitchType {
+        let stats = match self.get_stats().await {
+            Some(b) => b,
+            None => return SwitchType::Offline,
+        };
+
+        if let Some(offline) = triggers.offline {
+            if stats.kbps.recv_30s > 0 && stats.kbps.recv_30s <= offline.into() {
+                return SwitchType::Offline;
+            }
+        }
+
+        if stats.kbps.recv_30s == 0 {
+            return SwitchType::Previous;
+        }
+
+        if let Some(low) = triggers.low {
+            if stats.kbps.recv_30s <= low.into() {
+                return SwitchType::Low;
+            }
+        }
+
+        return SwitchType::Normal;
+    }
+}
+
+#[async_trait]
+#[typetag::serde]
+impl StreamServersCommands for SRS {
+    async fn bitrate(&self) -> super::Bitrate {
+        let stats = match self.get_stats().await {
+            Some(stats) => stats,
+            None => return super::Bitrate { message: None },
+        };
+
+        let message = format!("{} Kbps", stats.kbps.recv_30s);
+        super::Bitrate {
+            message: Some(message),
+        }
+    }
+
+    async fn source_info(&self) -> Option<String> {
+        let stats = self.get_stats().await?;
+
+        let bitrate = format!("{} Kbps", stats.kbps.recv_30s);
+
+        Some(format!("{}", bitrate))
+    }
+}
+
+#[typetag::serde]
+impl Bsl for SRS {
+    fn as_any_mut(&mut self) -> &mut dyn std::any::Any {
+        self
+    }
+}
